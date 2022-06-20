a8ae7da0f1d5f2db6fbd5b8b1dc41bbe15e836c5
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-20 16:24:31 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -1,9 +1,1 @@
-'use strict'
-
-const attr1 = document.currentScript.getAttribute('data-attr1');
-const attr2 = document.currentScript.getAttribute('data-attr2');
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('#attr1').innerHTML = attr1;
-    document.querySelector('#attr2').innerHTML = attr2;
-});
+document.write("teste");
