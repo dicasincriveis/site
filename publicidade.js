6b85f969007cdad61a1cf2b55646350b2a438703
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-20 20:20:30 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -1,5 +1,5 @@
 var url_atual = window.location.href;
-if (url_atual = http://www.dicasincriveis.com/2022/06/por-que-da-erro-quando-tento-pagar-com.html) {
+if (url_atual == http://www.dicasincriveis.com/2022/06/por-que-da-erro-quando-tento-pagar-com.html) {
     document.write("legal")
  else
    document.write("nada")
