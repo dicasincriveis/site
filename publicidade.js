86b2f97d8b5a8ddfe577ab71d33b72807ebd05c8
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-20 18:51:38 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -1,4 +1,4 @@
 document.write("<h1>Sejam bem vindos!!!</h1><p> Aqui é o melhor lugar para aprender. Volte sempre !</p>");
 document.write("teste");
-tag = 'teste2'
-document.write(tag)
+var url_atual = window.location.href;
+document.write(url_atual)
