011271e3b51008223e852925f8823ff24aeb7f4a
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-21 17:17:39 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -1,9 +1,9 @@
 var url_atual = window.location.href;
 document.write(url_atual);
-if (url_atual.toLowerCase().includes('erro') {
-    window.alert(5 + 6);
-} else if (url_atual.toLowerCase().includes('colesterol') {
-    document.write(<br>"colesterol encontrado">;
+if (url_atual.toLowerCase().includes('erro')) {
+    window.alert(5 + 6)
+} else if (url_atual.toLowerCase().includes('colesterol')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/mensagem.js"></script>')
 } else {
-    document.write('legal');
+    document.write('<p><h1>legal')
 }
