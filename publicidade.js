6b75428f07ee1fdcbcc16131158a0f5711a87d8b
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-21 17:35:26 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -1,9 +1,27 @@
 var url_atual = window.location.href;
 document.write(url_atual);
-if (url_atual.toLowerCase().includes('erro')) {
-    window.alert(5 + 6)
-} else if (url_atual.toLowerCase().includes('colesterol')) {
-    document.write('<script src="https://dicasincriveis.github.io/site/mensagem.js"></script>')
+if (url_atual.toLowerCase().includes('viagem')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/viagem.js"></script>')
+} else if (url_atual.toLowerCase().includes('finanças')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/financas.js"></script>')
+} else if (url_atual.toLowerCase().includes('casa')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/casa.js"></script>')
+} else if (url_atual.toLowerCase().includes('trabalho')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/trabalho.js"></script>')
+} else if (url_atual.toLowerCase().includes('relacionamentos')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/relacionamentos.js"></script>')
+} else if (url_atual.toLowerCase().includes('saude')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/saude.js"></script>')
+} else if (url_atual.toLowerCase().includes('saúde')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/saude.js"></script>')
+} else if (url_atual.toLowerCase().includes('emoções')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/emocoes.js"></script>')
+} else if (url_atual.toLowerCase().includes('emocoes')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/emocoes.js"></script>')
+} else if (url_atual.toLowerCase().includes('habitos')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/habitos.js"></script>')
+} else if (url_atual.toLowerCase().includes('hábitos')) {
+    document.write('<script src="https://dicasincriveis.github.io/site/habitos.js"></script>')
 } else {
-    document.write('<p><h1>legal')
+    document.write('<script src="https://dicasincriveis.github.io/site/geral.js"></script>')
 }
