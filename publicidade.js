5806c2f63e8012f5fc30865f898df89e2bf8188a
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-21 16:52:45 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -1,7 +1,1 @@
-var url_atual = window.location.href;
-document.write(url_atual);
-if (url_atual.toLowerCase().includes('erro') {
-    window.alert(5 + 6);
-} else {
-    document.write('legal')
-}
+document.write("teste");
