aa1028f7d5be628cf18175656182e94fa55ef8e6
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-21 17:05:07 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -1,11 +1,9 @@
-document.write("teste");
 var url_atual = window.location.href;
 document.write(url_atual);
-document.write(<br>)
 if (url_atual.toLowerCase().includes('erro') {
     window.alert(5 + 6);
 } else if (url_atual.toLowerCase().includes('colesterol') {
-    document.write(<br>"colesterol encontrado">
+    document.write(<br>"colesterol encontrado">;
 } else {
-    document.write('legal')
+    document.write('legal');
 }
