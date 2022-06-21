b39c8ff24f316dcfcdd53cf486bd4cc8ba947074
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-21 16:36:13 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -6,4 +6,10 @@ if (url_atual.toLowerCase().includes('erro') {
 } else {
     document.write("Legal")
 }
-
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
