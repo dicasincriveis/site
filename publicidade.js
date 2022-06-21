d8edd972ba06bb235b4a1b6fc5075e5f113b0610
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-06-20 21:11:02 -0300

    Update publicidade.js

--- publicidade.js
+++ publicidade.js
@@ -2,3 +2,4 @@ var url_atual = window.location.href;
 if (url_atual == 'http://www.dicasincriveis.com/2022/06/por-que-da-erro-quando-tento-pagar-com.html') {
     window.alert(5 + 6);
 }
+document.write(tag_name)
