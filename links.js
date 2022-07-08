49f427a700bcf28fde99e86fbb8e0c9bf22947b6
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-07-08 18:28:38 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,1 +1,5 @@
-document.write("teste")
+function censorText(){             
+    var explicit = document.getElementById("post").innerHTML;
+    var clean = explicit.replace(/relacionamentos|worse/gi,"teste");
+     document.getElementById("post").innerHTML = clean;
+}
