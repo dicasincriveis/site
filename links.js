951b1628837e5fd7b69b66e73fa8a41b6c1971e0
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-07-08 14:52:15 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,18 +1,8 @@
-document.addEventListener("DOMContentLoaded", function(){
-   var links = {
-      "relacionamentos": "teste.html",
-      "abaixo-assinado": "linkaabaixoassinado.html",
-      "ajuda": "linkajuda.html",
-      "linha": "linklinha.html"
-   }
-   
-   var bodi = document.querySelectorAll("body *:not(script)");
-   for(var x=0; x<bodi.length; x++){
-      var html = bodi[x].innerHTML;
-      for(var i in links){
-         var re = new RegExp("\\b(?![^<]*?>)("+i+")(?!>)", "gi");
-         html = html.replace(re, ' <a href="'+links[i]+'">$1</a>');
-      }
-      bodi[x].innerHTML = html;
-   }
-});
+function replace_text_wps($text) {
+    $text = str_replace('TEXT_TO_REPLACE', 'TEXT_YOU_WANT_TO_REPLACE_WITH', $text);
+    $text = str_replace('tech support', '<a href="/techsupport">Tech support</a>', $text);
+    $text = str_replace('computers', '<a href="/computers">Computers</a>', $text);
+    return $text;
+}
+ 
+add_filter('the_content', 'replace_text_wps');
