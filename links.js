fe782ab746c8a3e30e5997ef584f9f28d22113a4
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-07-08 16:27:39 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,2 +1,2 @@
-let text = document.getElementById("main").innerHTML; 
-document.getElementById("main").innerHTML = text.replace("relacionamentos", "W3Schools");
+let text = document.getElementById("meta-post").innerHTML; 
+document.getElementById("meta-post").innerHTML = text.replace("relacionamentos", "W3Schools");
