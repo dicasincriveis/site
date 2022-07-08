2347c696501c22891ba997434a24078559b71dc5
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-07-08 14:52:58 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,6 +1,6 @@
 function replace_text_wps($text) {
     $text = str_replace('TEXT_TO_REPLACE', 'TEXT_YOU_WANT_TO_REPLACE_WITH', $text);
-    $text = str_replace('tech support', '<a href="/techsupport">Tech support</a>', $text);
+    $text = str_replace('relacionamentos', '<a href="teste.html">relacionamentos</a>', $text);
     $text = str_replace('computers', '<a href="/computers">Computers</a>', $text);
     return $text;
 }
