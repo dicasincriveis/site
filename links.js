caec58dfa43c71e142e1645cf62f280c3969cdb9
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-07-08 15:35:35 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,11 +1,2 @@
-function replaceText(){
-    var theDiv = document.getElementById("myDiv");
-    var theText = theDiv .innerHTML;
-
-    // Replace words
-    theText = theText.replace("relacionamentos", "TESTE");
-    theText = theText.replace("one", "fish");
-    theText = theText.replace("tom", "drum");
-
-    theDiv.innerHTML = theText;
-}
+var heo1 = document.querySelector('.post-body');
+heo1.innerText = heo1.innerText.replace('relacionamentos',"chanquadi");
