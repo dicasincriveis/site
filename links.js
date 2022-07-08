3b756f501dfcf9938127056572a2f69ce0650411
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-07-08 15:03:11 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,8 +1,1 @@
-function replace_text_wps($text) {
-    $text = str_replace('TEXT_TO_REPLACE', 'TEXT_YOU_WANT_TO_REPLACE_WITH', $text);
-    $text = str_replace('relacionamentos', '<a href="teste.html">relacionamentos</a>', $text);
-    $text = str_replace('computers', '<a href="/computers">Computers</a>', $text);
-    return $text;
-}
- 
-add_filter('the_content', 'replace_text_wps');
+document.body.innerHTML = document.body.innerHTML.replace('relacionamentos', 'TESTE');
