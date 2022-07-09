538b4d17afbf7ee0ceeb16c58800487b40bbe912
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-07-09 10:07:09 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,2 +1,2 @@
 let text = document.getElementById("links").innerHTML; 
-document.getElementById("links").innerHTML = text.replace("relacionamentos",<a href="relacionamentos.html>"relacionamentos"</a>).replace("problema","PROBLEMA");
+document.getElementById("links").innerHTML = text.replace("relacionamentos",<a href='relacionamentos.html'>"relacionamentos"</a>).replace("problema","PROBLEMA");
