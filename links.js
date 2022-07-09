d5fd72256d1db7f78a65e2d8907eafcf965b67ad
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-07-09 10:25:29 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,2 +1,9 @@
 let text = document.getElementById("links").innerHTML; 
 document.getElementById("links").innerHTML = text.replace("relacionamentos","<a href='http://www.dicasincriveis.com/2022/07/relacionamento-o-que-e-um-relacionamento-ganha-ganha.html'>relacionamentos</a>").replace("problema","<a href='http://www.dicasincriveis.com/2021/06/simplifique-sua-vida.html'>problema</a>");
+
+
+//trecho do código
+//<div id='links'> (inserido)
+//<data:post.body/> (original)
+//</div> (inserido)
+//<script src='https://dicasincriveis.github.io/site/links.js'/> (inserido)
