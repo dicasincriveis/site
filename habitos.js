8f492ba6b267b3338cf66de0d36c3c2a89816b80
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-09-20 18:15:48 -0300

    Update habitos.js

--- habitos.js
+++ habitos.js
@@ -1,1 +1,3 @@
-document.write("<a href='https://pay.hotmart.com/Y71386648F?checkoutMode=10&bid=1663707081004'>habitos</a>")
+document.write("<a href='https://pay.hotmart.com/Y71386648F?checkoutMode=10&bid=1663707081004' target='new'>Curso Hábitos Que Levam ao Sucesso</a>")
+document.write("Estimule todo o seu potencial através da transformação de hábitos.<br>")
+document.write("Curso Online com mentoria.<br>">
