8a1acad0d4013896ebbe292aec75be0d7b40aecc
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-09-20 21:15:07 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 let text = document.getElementById("links").innerHTML; 
-document.getElementById("links").innerHTML = text.replace(" relacionamentos "," <a href='http://www.dicasincriveis.com/2022/07/relacionamento-o-que-e-um-relacionamento-ganha-ganha.html'>relacionamentos</a> ").replace("problemas"," <a href='http://www.dicasincriveis.com/2021/06/simplifique-sua-vida.html'>problemas</a> ");
+document.getElementById("links").innerHTML = text.replace(" relacionamentos "," <a href='http://www.dicasincriveis.com/2022/07/relacionamento-o-que-e-um-relacionamento-ganha-ganha.html'>relacionamentos</a> ").replace("problemas"," <a href='http://www.dicasincriveis.com/2021/06/simplifique-sua-vida.html'>problemas</a> ").text.replace(" hábitos "," <a href='https://www.dicasincriveis.com/p/habitos-que-levam-ao-sucesso.html'>hábitos</a> ");
 
 
 //trecho do código
