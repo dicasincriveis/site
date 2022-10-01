f639a00dddbc8372c2f923af98b4d741c163599d
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-09-30 22:39:02 -0300

    Update z_pub_artigo_viagens.js

--- z_pub_artigo_viagens.js
+++ z_pub_artigo_viagens.js
@@ -1,1 +1,1 @@
-document.write("<script async="" data-uid="6c35963153" src="https://colossal-originator-9399.ck.page/6c35963153/index.js"></script>")
+document.write("<script async='' data-uid='6c35963153' src='https://colossal-originator-9399.ck.page/6c35963153/index.js'></script>")
