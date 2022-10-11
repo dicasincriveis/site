bf3c54bfd7c63026b90c561e44dd01e005e67829
dicasincriveis <107642383+dicasincriveis@users.noreply.github.com>
2022-10-11 18:33:27 -0300

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 let text = document.getElementById("links").innerHTML; 
-document.getElementById("links").innerHTML = text.replace(" relacionamentos "," <a href='https://www.dicasincriveis.com/2022/07/relacionamento-o-que-e-um-relacionamento-ganha-ganha.html' style="color: #F00">relacionamentos</a> ").replace("problemas"," <a href='https://www.dicasincriveis.com/2021/06/simplifique-sua-vida.html' style="color: #F00">problemas</a> ").replace(" hábitos "," <a href='https://www.dicasincriveis.com/p/habitos-que-levam-ao-sucesso.html' style="color: #F00">hábitos</a> ");
+document.getElementById("links").innerHTML = text.replace(" relacionamentos "," <a href='https://www.dicasincriveis.com/2022/07/relacionamento-o-que-e-um-relacionamento-ganha-ganha.html' style='color: #F00'>relacionamentos</a> ").replace("problemas"," <a href='https://www.dicasincriveis.com/2021/06/simplifique-sua-vida.html' style='color: #F00'>problemas</a> ").replace(" hábitos "," <a href='https://www.dicasincriveis.com/p/habitos-que-levam-ao-sucesso.html' style='color: #F00'>hábitos</a> ");
 
 
 //trecho do código
